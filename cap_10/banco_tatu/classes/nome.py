9f28bc41b52302d70c78ab464750b3115aa887e4
davidade300 <iaderinho@icloud.com>
2025-06-17 00:43:08 -0300

    notas sobre propriedades e metodos estáticos adicionados

--- cap_10/banco_tatu/classes/nome.py
+++ cap_10/banco_tatu/classes/nome.py
@@ -1,7 +1,12 @@
+from functools import total_ordering
+
+
+@total_ordering
 class Nome:
     def __init__(self, nome: str) -> None:
         if nome is None or not nome.strip():
             raise ValueError("Nome não pode ser nulo nem em branco")
+
         self.nome = nome
         self.chave = nome.strip().lower()
 
@@ -9,7 +14,7 @@ class Nome:
         return self.nome
 
     def __repr__(self) -> str:  # melhor que o da dio LOL (so adaptar)
-        return f"<Classe {type(self).__name__} em 0x{id(self):x} Nome: {self.nome} Chave: {self.chave}>"
+        return f"<Classe {type(self).__name__} em 0x{id(self):x} Nome: {self.__nome} Chave: {self.__chave}>"
 
     def __eq__(self, other) -> bool:
         print("__eq__ chamado")
@@ -20,6 +25,22 @@ class Nome:
         print("__lt__ chamado")
 
         return self.nome < other.nome
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, valor):
+        if valor is None or not valor.strip():
+            raise ValueError("Nome não pode ser nulo nem em branco")
+
+        self.__nome = valor
+        self.__chave = Nome.CriaChave(valor)
+
+    @staticmethod
+    def CriaChave(nome: str) -> str:
+        return nome.strip().lower()
 
 
 d = Nome("David")
