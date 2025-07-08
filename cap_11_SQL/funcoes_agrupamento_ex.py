87e2333d734581b2099b65ec440fb991c84315b1
davidade300 <iaderinho@icloud.com>
2025-07-08 11:47:45 -0300

    codigos com exemplos de funcoes de agrupamento em SQL e having adicionados

---
+++ cap_11_SQL/funcoes_agrupamento_ex.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+print("Região Estados População  Mínima    Máxima      Média    Total (soma)")
+print("====== =======          ========= ========== ==========  ============")
+
+with sqlite3.connect("brasil.db") as conexao:
+    for regiao in conexao.execute(
+        "SELECT regiao, COUNT(*), MIN(populacao), MAX(populacao), AVG(populacao),SUM(POPULACAO) FROM estados GROUP BY regiao"
+    ):
+        print("{0:6} {1:7} {2:18,} {3:10,} {4:10,.0f} {5:13,}".format(*regiao))
+
+    print(
+        "\nBrasil: {0:6} {1:18,} {2:10,} {3:10,.0f} {4:13,}".format(
+            *conexao.execute(
+                "SELECT COUNT(*), MIN(populacao), MAX(populacao), AVG(populacao), SUM(POPULACAO) FROM estados"
+            ).fetchone()
+        )
+    )
